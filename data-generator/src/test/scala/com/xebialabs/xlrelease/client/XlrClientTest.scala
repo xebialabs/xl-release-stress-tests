cfb9795e591806549035e114f92c34d9f983f325
Florent Le Gall <flegall@xebia.fr>
2015-03-18 15:50:33 +0100

    Re-enabled tests

--- data-generator/src/test/scala/com/xebialabs/xlrelease/client/XlrClientTest.scala
+++ data-generator/src/test/scala/com/xebialabs/xlrelease/client/XlrClientTest.scala
@@ -16,77 +16,77 @@ class XlrClientTest extends UnitTestSugar with XlrJsonProtocol {
   val client = new XlrClient("http://localhost:5516")
 
   describe("XLR client") {
-//    it("should create a release") {
-//      val release = Release.build("Release004")
-//
-//      val createResponse = client.createRelease(release).futureValue
-//      createResponse.status shouldBe StatusCodes.OK
-//
-//      val removeResponse = client.removeCi(release.id).futureValue
-//      removeResponse.status shouldBe StatusCodes.NoContent
-//    }
-//
-//    it("should create a phase within release") {
-//      val release = Release.build("Release005")
-//
-//      val phase = Phase.build("Phase002", release.id)
-//
-//      val phaseResponse = for (
-//        releaseResponse <- client.createRelease(release);
-//        phaseResponse <- client.createPhase(phase)
-//      ) yield phaseResponse
-//
-//      phaseResponse.futureValue.status shouldBe StatusCodes.OK
-//
-//      client.removeCi(release.id)
-//    }
-//
-//    it("should create tasks") {
-//      val release = Release.build("Release102")
-//      val phase = Phase.build("Phase002", release.id)
-//
-//      val taskResponse = for (
-//        releaseResponse <- client.createRelease(release);
-//        phaseResponse <- client.createPhase(phase);
-//        taskResponse <- client.createTask(Task.build("Task002", phase.id))
-//      ) yield taskResponse
-//
-//      taskResponse.futureValue.status shouldBe StatusCodes.OK
-//
-//      client.removeCi(release.id)
-//    }
-//
-//    it("should create many releases in batches") {
-//      val range = 0 until 20
-//      val releases = range.map(id =>
-//        Release.build(s"Release$id")
-//      )
-//      val groups = releases.grouped(100).toSeq
-//
-//      val releaseResponsesFutures = groups.map {
-//        case group: Seq[Release] => client.createCis(group.toSeq)
-//      }
-//      expectSuccessfulResponses(releaseResponsesFutures)
-//
-//      val releaseRemovalFutures = releases.map(release => {
-//        client.removeCi(release.id)
-//      })
-//      expectSuccessfulResponses(releaseRemovalFutures)
-//    }
-//
-//    it("should create many releases") {
-//      val range = 0 until 20
-//      val releases = range.map(id =>
-//        Release.build(s"Release$id")
-//      )
-//      val releaseResponsesFutures = releases.map(client.createRelease)
-//      expectSuccessfulResponses(releaseResponsesFutures)
-//
-//      val releaseRemovalFutures = releases.map(release => {
-//        client.removeCi(release.id)
-//      })
-//      expectSuccessfulResponses(releaseRemovalFutures)
-//    }
+    it("should create a release") {
+      val release = Release.build("Release004")
+
+      val createResponse = client.createRelease(release).futureValue
+      createResponse.status shouldBe StatusCodes.OK
+
+      val removeResponse = client.removeCi(release.id).futureValue
+      removeResponse.status shouldBe StatusCodes.NoContent
+    }
+
+    it("should create a phase within release") {
+      val release = Release.build("Release005")
+
+      val phase = Phase.build("Phase002", release.id)
+
+      val phaseResponse = for (
+        releaseResponse <- client.createRelease(release);
+        phaseResponse <- client.createPhase(phase)
+      ) yield phaseResponse
+
+      phaseResponse.futureValue.status shouldBe StatusCodes.OK
+
+      client.removeCi(release.id)
+    }
+
+    it("should create tasks") {
+      val release = Release.build("Release102")
+      val phase = Phase.build("Phase002", release.id)
+
+      val taskResponse = for (
+        releaseResponse <- client.createRelease(release);
+        phaseResponse <- client.createPhase(phase);
+        taskResponse <- client.createTask(Task.build("Task002", phase.id))
+      ) yield taskResponse
+
+      taskResponse.futureValue.status shouldBe StatusCodes.OK
+
+      client.removeCi(release.id)
+    }
+
+    it("should create many releases in batches") {
+      val range = 0 until 20
+      val releases = range.map(id =>
+        Release.build(s"Release$id")
+      )
+      val groups = releases.grouped(100).toSeq
+
+      val releaseResponsesFutures = groups.map {
+        case group: Seq[Release] => client.createCis(group.toSeq)
+      }
+      expectSuccessfulResponses(releaseResponsesFutures)
+
+      val releaseRemovalFutures = releases.map(release => {
+        client.removeCi(release.id)
+      })
+      expectSuccessfulResponses(releaseRemovalFutures)
+    }
+
+    it("should create many releases") {
+      val range = 0 until 20
+      val releases = range.map(id =>
+        Release.build(s"Release$id")
+      )
+      val releaseResponsesFutures = releases.map(client.createRelease)
+      expectSuccessfulResponses(releaseResponsesFutures)
+
+      val releaseRemovalFutures = releases.map(release => {
+        client.removeCi(release.id)
+      })
+      expectSuccessfulResponses(releaseRemovalFutures)
+    }
 
     it("should import template from a file") {
       val createResponseFuture = client.importTemplate("/20-automated-tasks.xlr")
