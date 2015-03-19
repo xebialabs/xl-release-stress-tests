7caada90dfa709c2a92c5cb2f8ce688750d0a869
Florent Le Gall <flegall@xebia.fr>
2015-03-19 14:30:52 +0100

    REL-2232 Factor generateReleases()

--- data-generator/src/main/scala/com/xebialabs/xlrelease/generator/ReleasesGenerator.scala
+++ data-generator/src/main/scala/com/xebialabs/xlrelease/generator/ReleasesGenerator.scala
@@ -11,41 +11,47 @@ object ReleasesGenerator {
   val idCounter = new AtomicInteger()
 
   def generateCompletedReleases(amount: Int): Seq[Seq[Ci]] = {
-    val releases = (1 to amount).map( n => {
-      val releaseNumber = idCounter.incrementAndGet()
-      Release.build(s"Applications/Release$releaseNumber", s"Stress test completed release $n", "COMPLETED", n, amount)
-    })
-
-    releases.map( release => createReleaseContent(release, "COMPLETED") :+ release)
+    generateReleases(amount, "COMPLETED", (n) => s"Stress test completed release $n")
   }
 
   def generateTemplateReleases(amount: Int): Seq[Seq[Ci]] = {
-    val releases = (1 to amount).map( n => {
-      val releaseNumber = idCounter.incrementAndGet()
-      Release.build(s"Applications/Release$releaseNumber", s"Stress test template release $n", "TEMPLATE", n, amount)
-    })
-
-    releases.map( release => createReleaseContent(release, "PLANNED") :+ release)
+    generateReleases(amount, "TEMPLATE", (n) => s"Stress test template release $n")
   }
 
   def generateActiveReleases(amount: Int): Seq[Seq[Ci]] = {
-    val releases = (1 to amount).map( n => {
-      val releaseNumber = idCounter.incrementAndGet()
-      Release.build(s"Applications/Release$releaseNumber", s"Stress test active release $n", "IN_PROGRESS", n, amount)
-    })
-
-    releases.map( release => createReleaseContent(release, "IN_PROGRESS") :+ release)
+    generateReleases(amount, "IN_PROGRESS", (n) => s"Stress test active release $n")
   }
 
-  private def createReleaseContent(release: Release, status: String): Seq[Ci] = {
-    val phases: Seq[Phase] = (1 to phasesPerRelease).map( n => Phase.build(s"Phase$n", release.id, status))
+  def generateReleases(amount: Int, status: String, titleGenerator: (Int) => String): Seq[Seq[Ci]] = {
+    val releases = (1 to amount).map(n => {
+      val releaseNumber = idCounter.incrementAndGet()
+      Release.build(s"Applications/Release$releaseNumber", titleGenerator(n), status, n, amount)
+    })
+
+    releases.map(release => createReleaseContent(release) :+ release)
+  }
+
+  private def createReleaseContent(release: Release): Seq[Ci] = {
+    val phases: Seq[Phase] = (1 to phasesPerRelease).map(
+      n => Phase.build(s"Phase$n", release.id, phaseStatus(release)))
 
     val tasks: Seq[Task] = phases.flatMap( phase => {
-      (1 to tasksPerPhase).map( n => Task.build(s"Task$n", phase.id, status = status))
+      (1 to tasksPerPhase).map(
+        n => Task.build(s"Task$n", phase.id, taskStatus(phase)))
     })
 
     phases ++ tasks
   }
 
+  private def phaseStatus(release: Release): String = {
+    release.status match {
+      case "COMPLETED" => "COMPLETED"
+      case "TEMPLATE" => "PLANNED"
+      case "IN_PROGRESS" => "IN_PROGRESS"
+    }
+  }
 
+  private def taskStatus(phase: Phase): String = {
+    phase.status
+  }
 }
