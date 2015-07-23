c85171fd5ab55dfa25bbf4e1a29d034fcbf2730e
Mike Kotsur <miccots@gmail.com>
2015-07-23 10:20:52 +0200

    PLAT-143 supported multiple baseURLs

--- runner/src/main/scala/stress/package.scala
+++ runner/src/main/scala/stress/package.scala
@@ -1,13 +1,18 @@
+import com.typesafe.scalalogging.LazyLogging
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 import scala.language.{implicitConversions, postfixOps}
 
-package object stress {
-  
+package object stress extends LazyLogging {
+
   val nbUsers = Integer.getInteger("users", 10)
   val maxResponseSeconds = Integer.getInteger("maxResponseSeconds", 300)
-  val baseUrl = Option(System.getProperty("baseUrl")).getOrElse("http://localhost:5516")
+
+  private def getBaseUrls: Seq[String] = Option(System.getProperty("baseUrl")) match  {
+    case None => Seq("http://localhost:5516")
+    case Some(baseUrlString) => baseUrlString.split(",")
+  }
 
   val nbTeams = Integer.getInteger("teams", 10)
   val nbOps = Integer.getInteger("ops", 20)
@@ -17,9 +22,8 @@ package object stress {
   val password = Option(System.getProperty("password")).getOrElse("admin")
 
   val httpProtocol = http
-    .baseURL(baseUrl)
-    .acceptHeader("application/json")
-    .basicAuth(username, password)
-    .contentTypeHeader("application/json; charset=UTF-8")
-
+      .baseURLs(getBaseUrls.toList)
+      .acceptHeader("application/json")
+      .basicAuth(username, password)
+      .contentTypeHeader("application/json; charset=UTF-8").build
 }
