7351e8d2d744ba084de9408682c17853e418b45d
Bulat Yaminov <byaminov@xebialabs.com>
2017-09-17 20:06:33 +0200

    REL-5235 Make sure first release is put into first folder, not the second

--- data-generator/src/main/scala/com/xebialabs/xlrelease/generator/ReleasesAndFoldersGenerator.scala
+++ data-generator/src/main/scala/com/xebialabs/xlrelease/generator/ReleasesAndFoldersGenerator.scala
@@ -212,7 +212,7 @@ class ReleasesAndFoldersGenerator {
       val folderId = if (createdFolderIds.isEmpty) {
         "Applications"
       } else {
-        createdFolderIds(releaseNumber % createdFolderIds.size)
+        createdFolderIds((releaseNumber - 1) % createdFolderIds.size)
       }
       val releaseId = s"$folderId/Release_${transaction}_$releaseNumber"
 
